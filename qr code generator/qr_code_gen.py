3ed13fd99fba3e80e993cb3d3294253999125fef
faris-ai <faris.aliman90@gmail.com>
2023-10-23 16:23:22 +0700

    add qr code generator

---
+++ qr code generator/qr_code_gen.py
@@ -0,0 +1,10 @@
+import qrcode
+
+data = input("Input the data: ")
+
+img = qrcode.make(data)
+
+path = input("Input the path of directory that you want to save: ")
+file_name =input("Input the name of qr code png file: ")
+
+img.save(f'{path}\{file_name}.png')
